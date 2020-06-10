5fc5c99aea4c284e9c7838b51da97cd4d5f2c583
hsh <tmdgns9526@naver.com>
2020-06-10 20:39:18 +0900

    수정한 날짜 2020-06-010 20:39

--- app/src/main/java/com/example/witness/AlbumDetail.java
+++ app/src/main/java/com/example/witness/AlbumDetail.java
@@ -1,14 +1,53 @@
 package com.example.witness;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
+import static com.example.witness.AlbumMain.img;
+import static com.example.witness.AlbumMain.img_text;
+import static com.example.witness.AlbumMain.img_title;
+
 public class AlbumDetail extends AppCompatActivity {
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.album_detail);
+
+        ImageView imgView = findViewById(R.id.imgView);
+        TextView tvTitle = findViewById(R.id.tvTitle);
+        TextView tvText = findViewById(R.id.tvText);
+        ImageButton btnBack = findViewById(R.id.btnBack);
+
+        Intent intent = getIntent();
+        int imgID = intent.getIntExtra("IMGID", -1);
+
+        if(imgID == -1) {
+            Toast.makeText(AlbumDetail.this, "오류가 발생했습니다.", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            imgView.setImageBitmap(img.get(imgID));
+            tvTitle.setText(img_title.get(imgID));
+            tvText.setText(img_text.get(imgID));
+        }
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/witness/AlbumMain.java
+++ app/src/main/java/com/example/witness/AlbumMain.java
@@ -1,32 +1,41 @@
 package com.example.witness;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
 public class AlbumMain extends BaseActivity {
 
-    Integer[] abc = {R.drawable.couple1, R.drawable.couple2, R.drawable.couple3, R.drawable.couple4, R.drawable.couple5, R.drawable.couple6, R.drawable.couple7};
-    String abcd = "abcdeasdfasdf";
-
-    ArrayList<Bitmap> img;
-    ArrayList<String> img_title;
-    ArrayList<String> img_text;
+    public static ArrayList<Bitmap> img;
+    public static ArrayList<String> img_date;
+    public static ArrayList<String> img_title;
+    public static ArrayList<String> img_text;
+    ArrayList<Integer> deleteCheck;
+    int isDelete = 0;
+    int deleteWork = 0;
 
     public void setting() {
         img = new ArrayList<>();
+        img_date = new ArrayList<>();
         img_title = new ArrayList<>();
         img_text = new ArrayList<>();
 
@@ -39,18 +48,23 @@ public class AlbumMain extends BaseActivity {
         bitmap = BitmapFactory.decodeResource(this.getResources(), R.drawable.couple4);
         img.add(bitmap);
 
+        img_date.add("2018-05-08");
         img_title.add("성경 그리고 반지");
         img_text.add("성경이랑 반지 있는 그림 내용임");
 
+        img_date.add("2019-12-06");
         img_title.add("은하수");
         img_text.add("은하수 그림 내용임");
 
+        img_date.add("2020-04-15");
         img_title.add("석양");
         img_text.add("석양 그림 내용임");
 
+        img_date.add("2020-06-10");
         img_title.add("꽃밭");
         img_text.add("꽃밭 그림 내용임");
     }
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -59,10 +73,74 @@ public class AlbumMain extends BaseActivity {
 
         setting();
 
+        ImageButton imgSort = findViewById(R.id.imgSort);
+        ImageButton imgAdd = findViewById(R.id.imgAdd);
+        ImageButton imgDelete = findViewById(R.id.imgDelete);
+
         GridView gv = findViewById(R.id.gridView);
 
-        MyGridAdapter gAdapter = new MyGridAdapter(this);
+        final MyGridAdapter gAdapter = new MyGridAdapter(this);
         gv.setAdapter(gAdapter);
+
+        imgSort.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        imgAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        imgDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(deleteWork==0) {
+                    isDelete = 1;
+                    Toast.makeText(AlbumMain.this, "삭제할 이미지를 선택해주세요", Toast.LENGTH_SHORT).show();
+                    deleteWork=1;
+                }
+                else {
+                    //삭제하기
+                    int count=0;
+                    for(int i=0; i<deleteCheck.size(); i++) {
+                        if(deleteCheck.get(i) == 1) {
+                            count++;
+                        }
+                    }
+
+                    AlertDialog.Builder dlg = new AlertDialog.Builder(AlbumMain.this);
+                    dlg.setTitle("삭제");
+                    dlg.setMessage(count + "개의 앨범을 삭제하시겠습니까?");
+                    dlg.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            for(int i=0; i<deleteCheck.size(); i++) {
+                                if(deleteCheck.get(i) == 1) {
+                                    img.remove(i);
+                                    img_date.remove(i);
+                                    img_title.remove(i);
+                                    img_text.remove(i);
+                                    gAdapter.notifyDataSetChanged();
+                                }
+                            }
+                        }
+                    });
+
+                    dlg.setNegativeButton("취소", null);
+                    dlg.show();
+
+                    deleteWork=0;
+                    for(int i=0; i<deleteCheck.size(); i++) {
+                        deleteCheck.add(i, 0);
+                    }
+                }
+            }
+        });
     }
 
     public class MyGridAdapter extends BaseAdapter {
@@ -74,7 +152,7 @@ public class AlbumMain extends BaseActivity {
 
         @Override
         public int getCount() {
-            return 0;
+            return img.size();
         }
 
         @Override
@@ -89,13 +167,37 @@ public class AlbumMain extends BaseActivity {
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            ImageView ivG = new ImageView(context);
-            ivG.setLayoutParams(new GridView.LayoutParams(250, 300));
+            final ImageView ivG = new ImageView(context);
+            ivG.setLayoutParams(new GridView.LayoutParams(300, 300));
             ivG.setScaleType(ImageView.ScaleType.FIT_CENTER);
-            ivG.setPadding(5,5,5,5);
+            ivG.setPadding(3, 3, 3, 3);
 
-            //ivG.setImageBitmap(img.get(position));
-            ivG.setImageResource(abc[position]);
+            ivG.setImageBitmap(img.get(position));
+
+            deleteCheck = new ArrayList<>();
+            for (int i = 0; i < img.size(); i++) {
+                deleteCheck.add(0);
+            }
+            final int pos = position;
+            ivG.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (isDelete == 0) {
+                        Intent intent = new Intent(AlbumMain.this, AlbumDetail.class);
+                        intent.putExtra("IMGID", pos);
+
+                        startActivity(intent);
+                    } else {
+                        if (deleteCheck.get(pos) == 0) {
+                            ivG.setColorFilter(Color.parseColor("#88000000"));
+                            deleteCheck.add(pos, 1);
+                        } else {
+                            ivG.setColorFilter(null);
+                            deleteCheck.add(pos, 0);
+                        }
+                    }
+                }
+            });
 
             return ivG;
         }
